658c0e08592a80e1a1d88d65b63969a0c2e13b6e
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-18 18:16:07 +0100

    add use state and maping throw the index

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
 import Create from './Create'
+import { useState } from 'react'
 
 const Home = () => {
-  return (
-    <div>
-        <h2>ToDo List</h2>
-        <Create />
-      
-    </div>
-  )
+    const [todos, setTodos] = useState([])
+  
+    return (
+        <div>
+            <h2>ToDo List</h2>
+            <Create />
+            {
+                todos.length === 0 
+                ? <div><h2>No Record</h2></div> 
+                : todos.map((todo, index) => (
+                    <div key={index}>
+                        {todo}
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default Home
- 
-
-
-
-
-
-
