b33d971ead35876fe272c06c8dc7a1fcff183f4b
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-19 18:00:19 +0100

    add momgoose scheme

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect('mongodb+srv://aliibrahimwork:JB272H6GHfO8TpzC@cluster0.5rpiq.mongodb.net/')
+
 
 
 app.post('/add', (req, res) => {
