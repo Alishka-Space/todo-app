30c6a381ac721f197162b13eb33c405260df0348
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-19 16:37:01 +0100

    usestate and target onchange

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,10 +1,17 @@
 
 
 const Create = () => {
+  const [task, setTask] = useState()
+
+
+
+  const handleAdd = () => {
+
+  }
   return (
     <div className="home">
     <form className="create_form">
-        <input type="text" placeholder="Enter Task" />
+        <input type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value) } />
         <button type="button" onClick={handleAdd} >submit</button>
     </form>
 </div>
