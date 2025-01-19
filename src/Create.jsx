da7bfff0536d099abcfa40befbc3a9711f86b354
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-19 16:26:38 +0100

    add handle onclick to button

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,8 +4,8 @@ const Create = () => {
   return (
     <div className="home">
     <form className="create_form">
-        <input type="text" placeholder="Add a todo" />
-        <button type="submit">Add</button>
+        <input type="text" placeholder="Enter Task" />
+        <button type="button" onClick={handleAdd} >submit</button>
     </form>
 </div>
   )
