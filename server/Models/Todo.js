b3da9aba46d5b3079f10bdd6b34b5d1bd26340e9
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-20 20:28:46 +0100

    add todos model ans mongoos schema

--- server/Models/Todo.js
+++ server/Models/Todo.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
 
+const todoSchema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true
+    }
+})
+
+const TodoModel = mongoose.model('Todos', todoSchema);
+
+module.exports = TodoModel;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require('mongoose');
 const cors = require('cors');
+const TodoModel = require('./Models/Todo');
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://aliibrahimwork:JB272H6GHfO8TpzC@cluster0.5rpiq.mongodb.net/')
+mongoose.connect(process.env.REACT_APP_MONGO_URI)
+// mongoose.connect('mongodb+srv://aliibrahimwork:JB272H6GHfO8TpzC@cluster0.5rpiq.mongodb.net/')
 
 
 
 app.post('/add', (req, res) => {
     const task = req.body.task;
-    console.log(task);
+    TodoModel.create({ 
+        task: task 
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+    
 
 })
 
