f0dfeace35d7f3a4bcc646564d8ece94a33af677
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-21 14:10:36 +0100

    add schema iscompleted

--- server/Models/Todo.js
+++ server/Models/Todo.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
-    task: {
-        type: String,
-        done: {
-            type: Boolean,
-            default: false
-        }
+    task: String,
+    isCompleted: {
+        type: Boolean,
+        default: false
     }
+
 })
 
 const TodoModel = mongoose.model('Todos', todoSchema);
