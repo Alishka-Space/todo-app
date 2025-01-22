a5b9088c87cc7522a8c42f48f5f4a134849a623b
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-22 20:44:26 +0100

    Deploy Todo App

--- server/index.js
+++ server/index.js
@@ -1,59 +1,63 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const cors = require('cors');
-const TodoModel = require('./Models/Todo');
+const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
+const TodoModel = require("./Models/Todo");
+
 dotenv.config();
 const app = express();
 
-
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.REACT_APP_MONGO_URI)
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get('/get', (req,res) =>{
-    TodoModel.find()
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-} )
 
-app.post('/add', (req, res) => {
-    const task = req.body.task;
-    TodoModel.create({ 
-        task: task 
-    }).then(result => res.json(result))
-    .catch(err => res.json(err))
-    
+// Routes
+app.get("/get", (req, res) => {
+  TodoModel.find()
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
-})
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+  TodoModel.create({ task: task })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
-app.put('/update/:id', (req, res) => {
-    const {id} = req.params;
-    TodoModel.findByIdAndUpdate({_id: id}, { isCompleted: true })
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndUpdate({ _id: id }, { isCompleted: true })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
-app.delete('/delete/:id', (req, res) => {
-    const {id} = req.params;
-    TodoModel.findByIdAndDelete({_id: id})
-    .then(result => res.json(result))
-    .catch(err => res.json(err))
-})
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  TodoModel.findByIdAndDelete({ _id: id })
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
 
-const path = require('path');
+// Serve Static Files in Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "dist")));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  });
 }
 
+// Server Configuration
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
