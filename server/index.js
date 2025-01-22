42c6c99efbde39d15421566b3df337dc09efbce3
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-22 13:10:12 +0100

    Added DELETE route to remove todos by ID

--- server/index.js
+++ server/index.js
@@ -36,6 +36,13 @@ app.put('/update/:id', (req, res) => {
     .catch(err => res.json(err))
 })
 
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id: id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
