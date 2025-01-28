66401be9f4fe3eddcf6f58518b8cffebfc4b08b2
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2025-01-28 13:03:22 +0100

    add

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
+  
 
 
 // Routes
